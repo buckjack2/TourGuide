106b95f2d300c0a1e3b4419fdb17b8a3e6443043
buckjack2 <buckjack@abv.bg>
2020-05-30 21:34:00 +0300

    2

---
+++ app/src/main/java/com/buckjack/tourguide/Monument.java
@@ -0,0 +1,32 @@
+package com.buckjack.tourguide;
+
+
+public class Monument {
+
+    //Monument Name
+    private String name;
+    //Monument Description
+    private String description;
+    //Monument Image Resource Id
+    private int imageResourceId;
+
+    //Constructor
+    public Monument(String name , String description , int imageResourceId){
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    //Getter Methods
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getImageResourceId(){
+        return imageResourceId;
+    }
+}
---
+++ app/src/main/java/com/buckjack/tourguide/ViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.buckjack.tourguide;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final String[] tabsTitle =  new String[]{"Pharaonic","Religious","Modern","Other"};
+
+    public ViewPagerAdapter(FragmentManager fm) {
+
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new PharaonicFragment();
+            case 1:
+                return new ReligiousFragment();
+            case 2:
+                return new ModernFragment();
+            default:
+                return new OtherStatuesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabsTitle.length;
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabsTitle[position];
+    }
+}
